2c5b9a5d3736fe84849bbb2c38bbbab921cd98d9
rinat <atkbrc354@mail.ru>
2022-10-24 15:59:54 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+
+let left_arrow = document.getElementById('left_arrow'),
+    right_arrow = document.getElementById('right_arrow');
+let slider_intro = document.querySelector('.slider_wrapper');
+let arrows = document.querySelector('.arrows');
+console.log(right_arrow);
